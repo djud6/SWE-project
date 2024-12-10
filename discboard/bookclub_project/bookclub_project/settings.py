35b19588f2ead65425f779de33845111ff98add5
djud6 <134981651+djud6@users.noreply.github.com>
2024-12-10 18:37:20 -0500

    Update settings.py

--- discboard/bookclub_project/bookclub_project/settings.py
+++ discboard/bookclub_project/bookclub_project/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'discussion.apps.DiscussionConfig',
     'members.apps.MembersConfig',
-    'widget_tweaks',
 ]
 
 AUTH_USER_MODEL = 'discussion.User'
